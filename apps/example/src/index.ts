3abf22b55d2d09e18d562ab3847c3528098a73fc
tf63 <74246282+tf63@users.noreply.github.com>
2024-08-13 06:14:50 +0900

    feat: setup vite

--- apps/example/src/index.ts
+++ apps/example/src/index.ts
@@ -1,8 +1,11 @@
-import { foo } from '@repo/tsup'
+import { foo as foo_tsup } from '@repo/tsup'
+import { foo as foo_vite } from '@repo/vite'
 
 const main = () => {
     // biome-ignore lint/suspicious/noConsoleLog: <explanation>
-    console.log(foo)
+    console.log(`tsup: ${foo_tsup}`)
+    // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+    console.log(`vite: ${foo_vite}`)
 }
 
 main()
